ecca6c7893b2d61967c11b7fb48078187bc4d1d0
Ignis144 <51386204+Ignis144@users.noreply.github.com>
2023-03-06 11:08:49 +0100

    Recipe updates

--- src/main/java/com/ignis/igrobotics/core/util/ItemStackUtils.java
+++ src/main/java/com/ignis/igrobotics/core/util/ItemStackUtils.java
@@ -1,5 +1,6 @@
 package com.ignis.igrobotics.core.util;
 
+import com.google.gson.JsonElement;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -7,6 +8,7 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.items.IItemHandler;
 
 import javax.annotation.Nullable;
+import javax.json.JsonObject;
 import java.util.ArrayList;
 import java.util.function.Predicate;
 
