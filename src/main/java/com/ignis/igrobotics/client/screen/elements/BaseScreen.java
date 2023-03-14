92696603179173b7b9f76c5999cca3fa548bcd70
Ignis144 <51386204+Ignis144@users.noreply.github.com>
2023-03-14 23:33:20 +0100

    Screen IElement implementation

---
+++ src/main/java/com/ignis/igrobotics/client/screen/elements/BaseScreen.java
@@ -0,0 +1,86 @@
+package com.ignis.igrobotics.client.screen.elements;
+
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.*;
+
+public class BaseScreen extends Screen implements IElement {
+
+    private int x, y;
+    private boolean visible, enabled;
+
+    private IElement parentElement;
+
+    protected BaseScreen(Component pTitle) {
+        super(pTitle);
+    }
+
+    @Override
+    public void setX(int x) {
+        for(GuiEventListener b : children()) {
+            if(!(b instanceof IElement)) continue;
+            IElement element = (IElement) b;
+            element.setX(element.getShape().x + x - this.x);
+        }
+        this.x = x;
+    }
+
+    @Override
+    public void setY(int y) {
+        for(GuiEventListener b : children()) {
+            if(!(b instanceof IElement)) continue;
+            IElement element = (IElement) b;
+            element.setY(element.getShape().y + y - this.y);
+        }
+        this.y = y;
+    }
+
+    @Override
+    public Rectangle getShape() {
+        return new Rectangle(x, y, width, height);
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isVisible() {
+        return visible;
+    }
+
+    @Override
+    public void setParentComponent(IElement comp) {
+        this.parentElement = comp;
+    }
+
+    @Override
+    public @Nullable IElement getParentComponent() {
+        return parentElement;
+    }
+
+    @Override
+    public NarrationPriority narrationPriority() {
+        return NarrationPriority.NONE;
+    }
+
+    @Override
+    public void updateNarration(NarrationElementOutput pNarrationElementOutput) {
+
+    }
+}
