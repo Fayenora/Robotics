8595ad9ae053fee47917ee88caab53cbd098c6ad
rolf.stueven <rolf.stueven@stud.uni-goettingen.de>
2023-03-25 21:17:25 +0100

    Opening Menu

--- src/main/java/com/ignis/igrobotics/common/RobotBehavior.java
+++ src/main/java/com/ignis/igrobotics/common/RobotBehavior.java
@@ -4,6 +4,7 @@ public class RobotBehavior {
     @SubscribeEvent
     public static void onRobotRightClick(EntityInteractSpecific event) {
         if(event.getPlayer().level.isClientSide()) return;
-        //TODO Open robot screen
+        if(!event.getTarget.getCapability(ModCapabilities.ROBOT).isPresent()) return;
+        NetworkHooks.openScreen(event.getPlayer(), new SimpleMenuProvider((id, playerInv, player) -> new RobotMenu(id, playerInv, Lang.localise("container.robot"))));
     }
 }
