39651b24ebd0a74b00eaed2d912593b1cc04f7a3
Ignis144 <rolf@rednoses.de>
2023-06-01 12:41:57 +0200

    Temporarily moved default configs to resources

--- src/main/java/com/ignis/igrobotics/integration/config/ConfigUtils.java
+++ src/main/java/com/ignis/igrobotics/integration/config/ConfigUtils.java
@@ -2,20 +2,26 @@ package com.ignis.igrobotics.integration.config;
 
 import com.google.gson.Gson;
 import com.ignis.igrobotics.Robotics;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.Resource;
 import org.apache.commons.compress.utils.IOUtils;
 
 import javax.annotation.Nullable;
 import java.io.*;
+import java.util.Optional;
 
 public class ConfigUtils {
 
     public static void copyFromDefault(String defaultConfigFile, File to) {
         try {
-            String path = "data/" + Robotics.MODID + "/configs/" + defaultConfigFile;
-            InputStream in = Robotics.class.getClassLoader().getResourceAsStream(path);
-            if(in == null) {
+            ResourceLocation path = new ResourceLocation(Robotics.MODID, "configs/" + defaultConfigFile);
+            //FIXME: Servers need to read the file from a data pack!
+            Optional<Resource> resource = Minecraft.getInstance().getResourceManager().getResource(path);
+            if(resource.isEmpty()) {
                 throw new RuntimeException("Unable to copy file! No file found at " + path);
             }
+            InputStream in = resource.get().open();
             FileOutputStream out = new FileOutputStream(to);
             IOUtils.copy(in, out);
             in.close();
