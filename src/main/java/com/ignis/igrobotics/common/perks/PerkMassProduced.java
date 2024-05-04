e0b0dea9f0b58efa554a97b1a79b18a97861f525
Ignis144 <rolf@rednoses.de>
2024-05-04 18:58:30 +0200

    Refactor

--- src/main/java/com/ignis/igrobotics/common/perks/PerkMassProduced.java
+++ src/main/java/com/ignis/igrobotics/common/perks/PerkMassProduced.java
@@ -43,38 +43,38 @@ public class PerkMassProduced extends Perk {
 
 	@Override
 	public void onEntityUpdate(int level, Mob entity, SimpleDataManager values) {
-		entity.getCapability(ModCapabilities.ROBOT).ifPresent(robot -> {
-			if(!robot.isActive()) return;
+		if(!entity.getCapability(ModCapabilities.ROBOT).isPresent()) return;
+		IRobot robot = entity.getCapability(ModCapabilities.ROBOT).resolve().get();
+		if(!robot.isActive()) return;
 
-			values.increment(TICK_COUNTER);
-			if(values.get(TICK_COUNTER) < UPDATE_RATE) return;
-			if(values.get(TICK_COUNTER) > UPDATE_RATE) { //Leave one tick in between for every robot to update
-				values.set(TICK_COUNTER, 0);
-				return;
-			}
+		values.increment(TICK_COUNTER);
+		if(values.get(TICK_COUNTER) < UPDATE_RATE) return;
+		if(values.get(TICK_COUNTER) > UPDATE_RATE) { //Leave one tick in between for every robot to update
+			values.set(TICK_COUNTER, 0);
+			return;
+		}
 
-			BlockPos lower = entity.blockPosition().relative(Direction.DOWN, AREA_SIZE).relative(Direction.SOUTH, AREA_SIZE).relative(Direction.EAST, AREA_SIZE);
-			BlockPos upper = entity.blockPosition().relative(Direction.UP, AREA_SIZE).relative(Direction.NORTH, AREA_SIZE).relative(Direction.WEST, AREA_SIZE);
-			AABB area = new AABB(lower, upper);
-			int allies = entity.level().getEntities(entity, area, ent -> {
-				Optional<IRobot> otherRobot = entity.getCapability(ModCapabilities.ROBOT).resolve();
-				Optional<IPerkMapCap> perkMap = entity.getCapability(ModCapabilities.PERKS).resolve();
-				if(otherRobot.isEmpty() || !otherRobot.get().isActive()) return false;
-				if(!RobotBehavior.hasAccess(robot.getOwner(), ent, EnumPermission.ALLY)) return false;
-				return perkMap.isPresent() && perkMap.get().contains(RoboticsConfig.current().perks.PERK_MASS_PRODUCED);
-			}).size();
+		BlockPos lower = entity.blockPosition().relative(Direction.DOWN, AREA_SIZE).relative(Direction.SOUTH, AREA_SIZE).relative(Direction.EAST, AREA_SIZE);
+		BlockPos upper = entity.blockPosition().relative(Direction.UP, AREA_SIZE).relative(Direction.NORTH, AREA_SIZE).relative(Direction.WEST, AREA_SIZE);
+		AABB area = new AABB(lower, upper);
+		int allies = entity.level().getEntities(entity, area, ent -> {
+			Optional<IRobot> otherRobot = entity.getCapability(ModCapabilities.ROBOT).resolve();
+			Optional<IPerkMapCap> perkMap = entity.getCapability(ModCapabilities.PERKS).resolve();
+			if(otherRobot.isEmpty() || !otherRobot.get().isActive()) return false;
+			if(!RobotBehavior.hasAccess(robot.getOwner(), ent, EnumPermission.ALLY)) return false;
+			return perkMap.isPresent() && perkMap.get().contains(RoboticsConfig.current().perks.PERK_MASS_PRODUCED);
+		}).size();
 
-			if(allies <= 1) return; //No allies but itself
-			allies = Math.min(allies, MAX_ALLIES);
-			allies = (int) Math.ceil((double) allies / 2); //Effects only increase every 2 allies and are capped at 8 allies
-			int duration_ticks = UPDATE_RATE * PerkBehavior.PERK_TICK_RATE * 2 + 40;
+		if(allies <= 1) return; //No allies but itself
+		allies = Math.min(allies, MAX_ALLIES);
+		allies = (int) Math.ceil((double) allies / 2); //Effects only increase every 2 allies and are capped at 8 allies
+		int duration_ticks = UPDATE_RATE * PerkBehavior.PERK_TICK_RATE * 2 + 40;
 
-			for(MobEffect effect : effectStrengths.keySet()) {
-				int amplifier = effectStrengths.get(effect)[level - 1][allies];
-				if(amplifier <= 0) continue;
-				entity.addEffect(new MobEffectInstance(effect, duration_ticks, amplifier, true, false));
-			}
-		});
+		for(MobEffect effect : effectStrengths.keySet()) {
+			int amplifier = effectStrengths.get(effect)[level - 1][allies];
+			if(amplifier <= 0) continue;
+			entity.addEffect(new MobEffectInstance(effect, duration_ticks, amplifier - 1, true, false));
+		}
 	}
 
 	@Override
