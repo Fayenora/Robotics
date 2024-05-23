5336f38f001a9e7b06223f3c12ea36e1f81b38bb
Ignis144 <rolf@rednoses.de>
2024-05-23 11:24:09 +0200

    FIX: Counted Ingredients
    - Left to Right conversion recipes

--- src/main/java/com/ignis/igrobotics/core/CountedIngredient.java
+++ src/main/java/com/ignis/igrobotics/core/CountedIngredient.java
@@ -1,9 +1,12 @@
 package com.ignis.igrobotics.core;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.ignis.igrobotics.Robotics;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.crafting.IIngredientSerializer;
 import net.minecraftforge.common.crafting.VanillaIngredientSerializer;
@@ -11,22 +14,46 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 @MethodsReturnNonnullByDefault
 public class CountedIngredient extends Ingredient {
 
     public static final CountedIngredientSerializer SERIALIZER = new CountedIngredientSerializer();
+    private final int amount;
 
-    protected CountedIngredient(Ingredient ingredient) {
-        super(Arrays.stream(ingredient.getItems()).filter((stack) -> {
-            return !stack.isEmpty();
-        }).map(Ingredient.ItemValue::new));
+    protected CountedIngredient(Ingredient ingredient, int amount) {
+        super(Arrays.stream(ingredient.getItems()).filter(stack -> !stack.isEmpty()).map(Ingredient.ItemValue::new));
+        this.amount = amount;
+    }
+
+    public CountedIngredient(Value ingredient, int amount) {
+        super(Stream.of(ingredient));
+        this.amount = amount;
     }
 
     @Override
     @SuppressWarnings("DataFlowIssue")
     public boolean test(@Nullable ItemStack stack) {
-        return super.test(stack) && stack.getCount() >= getCount(this);
+        return super.test(stack) && stack.getCount() >= amount;
+    }
+
+    @Override
+    public ItemStack[] getItems() {
+        ItemStack[] stacks = super.getItems();
+        for(ItemStack stack : stacks) {
+            stack.setCount(amount);
+        }
+        return stacks;
+    }
+
+    @Override
+    public JsonElement toJson() {
+        JsonObject json = new JsonObject();
+        json.addProperty("type", Robotics.rl("counted").toString());
+        json.add("items", super.toJson());
+        json.addProperty("amount", this.amount);
+        return json;
     }
 
     @Override
@@ -43,26 +70,25 @@ public class CountedIngredient extends Ingredient {
     private static class CountedIngredientSerializer implements IIngredientSerializer<CountedIngredient> {
         @Override
         public CountedIngredient parse(FriendlyByteBuf buffer) {
-            return (CountedIngredient) VanillaIngredientSerializer.INSTANCE.parse(buffer);
+            return new CountedIngredient(VanillaIngredientSerializer.INSTANCE.parse(buffer), buffer.readInt());
         }
 
         @Override
         public CountedIngredient parse(JsonObject json) {
-            if(!json.isJsonObject()) return new CountedIngredient(Ingredient.EMPTY);
+            if(!json.isJsonObject()) return new CountedIngredient(new ItemValue(new ItemStack(Items.BARRIER)), 1);
             JsonObject jsonObject = json.getAsJsonObject();
-            CountedIngredient ingredient = new CountedIngredient(Ingredient.fromJson(jsonObject.get("items")));
+            Value ingredient = Ingredient.valueFromJson(jsonObject.get("items").getAsJsonObject());
+            int amount = 1;
             if(jsonObject.has("amount")) {
-                int amount = json.getAsJsonObject().get("amount").getAsInt();
-                for(ItemStack stack : ingredient.getItems()) {
-                    stack.setCount(amount);
-                }
+                amount = jsonObject.get("amount").getAsInt();
             }
-            return ingredient;
+            return new CountedIngredient(ingredient, amount);
         }
 
         @Override
         public void write(FriendlyByteBuf buffer, CountedIngredient ingredient) {
             VanillaIngredientSerializer.INSTANCE.write(buffer, ingredient);
+            buffer.writeInt(ingredient.amount);
         }
     }
 }
--- src/main/java/com/ignis/igrobotics/datagen/RecipeGenerator.java
+++ src/main/java/com/ignis/igrobotics/datagen/RecipeGenerator.java
@@ -1,16 +1,16 @@
 package com.ignis.igrobotics.datagen;
 
 import com.ignis.igrobotics.Reference;
+import com.ignis.igrobotics.Robotics;
 import com.ignis.igrobotics.common.recipes.AssemblerRecipeBuilder;
 import com.ignis.igrobotics.common.recipes.WireCutterRecipeBuilder;
+import com.ignis.igrobotics.core.CountedIngredient;
 import com.ignis.igrobotics.core.robot.EnumRobotMaterial;
 import com.ignis.igrobotics.core.robot.EnumRobotPart;
 import com.ignis.igrobotics.definitions.ModItems;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.FinishedRecipe;
-import net.minecraft.data.recipes.RecipeCategory;
-import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.Tags;
 import org.jetbrains.annotations.NotNull;
@@ -60,6 +60,19 @@ public class RecipeGenerator extends RecipeProvider {
                     .pattern("pgp")
                     .save(writer);
 
+            ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.MATERIALS[material.getID() - 1][2].get())
+                    .unlockedBy(getHasName(plate), has(plate))
+                    .requires(ModItems.MATERIALS[material.getID() - 1][3].get()).save(writer, Robotics.MODID + ":" + material.getName() + "_left_to_right_arm");
+            ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.MATERIALS[material.getID() - 1][3].get())
+                    .unlockedBy(getHasName(plate), has(plate))
+                    .requires(ModItems.MATERIALS[material.getID() - 1][2].get()).save(writer, Robotics.MODID + ":" + material.getName() + "_right_to_left_arm");
+            ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.MATERIALS[material.getID() - 1][4].get())
+                    .unlockedBy(getHasName(plate), has(plate))
+                    .requires(ModItems.MATERIALS[material.getID() - 1][5].get()).save(writer, Robotics.MODID + ":" + material.getName() + "_left_to_right_leg");
+            ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.MATERIALS[material.getID() - 1][5].get())
+                    .unlockedBy(getHasName(plate), has(plate))
+                    .requires(ModItems.MATERIALS[material.getID() - 1][4].get()).save(writer, Robotics.MODID + ":" + material.getName() + "_right_to_left_leg");
+
             defineLimb(material, EnumRobotPart.RIGHT_ARM)
                     .pattern("mp ")
                     .pattern("pwp")
@@ -87,8 +100,8 @@ public class RecipeGenerator extends RecipeProvider {
 
         int i = 0;
         for(EnumRobotMaterial material : Reference.WIRE_METALS) {
-            new WireCutterRecipeBuilder(ModItems.WIRES[i++].get())
-                    .input(material.getMetal())
+            new WireCutterRecipeBuilder(ModItems.WIRES[i++].get(), 3)
+                    .input(new CountedIngredient(new Ingredient.TagValue(material.getMetal()), 2))
                     .energyRequirement(10000 * material.getStiffness())
                     .processingTime(100)
                     .save(writer);
@@ -99,6 +112,7 @@ public class RecipeGenerator extends RecipeProvider {
         ItemLike plate = ModItems.PLATES[material.getID() - 1].get();
         return ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.MATERIALS[material.getID() - 1][part.getID()].get())
                 .unlockedBy(getHasName(plate), has(plate))
+                .showNotification(false)
                 .define('m', ModItems.SERVO_MOTOR.get())
                 .define('p', plate)
                 .define('w', ModItems.WIRING.get());
