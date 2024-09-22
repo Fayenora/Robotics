8ed8da6912a17a204416afba95a8e5220c377f29
Ignis144 <faye@nora.simplelogin.com>
2024-09-22 19:15:31 +0200

    FIX: Pehkui Compat

--- src/main/java/com/io/norabotics/mixins/OpenerCounter.java
+++ src/main/java/com/io/norabotics/mixins/OpenerCounter.java
@@ -6,8 +6,10 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.ContainerOpenersCounter;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ContainerOpenersCounter.class)
 public class OpenerCounter {
@@ -16,18 +18,18 @@ public class OpenerCounter {
     private int openCount;
 
     /**
-     * Vanilla regularly rechecks how many players opened a container. However, it only considers players capable of opening things. 
-     * Solution: Prevent the rechecks by not actually calculating how many players are standing around the container and viewing the menu, 
+     * Vanilla regularly rechecks how many players opened a container. However, it only considers players capable of opening things.
+     * Solution: Prevent the rechecks by not actually calculating how many players are standing around the container and viewing the menu,
      * but simply returning the count gained from {@link ContainerOpenersCounter#incrementOpeners(Player, Level, BlockPos, BlockState)} & {@link ContainerOpenersCounter#decrementOpeners(Player, Level, BlockPos, BlockState)}
      * This prevents any actual rechecks in {@link ContainerOpenersCounter#recheckOpeners(Level, BlockPos, BlockState)}
-     * @reason Method cannot be cancelled
-     * @author io.44
+     *
      * @param level the world
-     * @param pos the position of the container
-     * @return amount of players/entities that opened that container
+     * @param pos   the position of the container
+     * @param cir   CallbackInfo provided by mixin
+     * @author io.44
      */
-    @Overwrite
-    private int getOpenCount(Level level, BlockPos pos) {
-        return openCount;
+    @Inject(method = "getOpenCount", at = @At("HEAD"), cancellable = true)
+    private void robotics$getOpenCount(Level level, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
+        cir.setReturnValue(openCount);
     }
 }
